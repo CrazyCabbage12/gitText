81817335bbe9415906adbf49ab842c98925be7d5
CrazyCabbage <CrazyCabbage0212@gmail.com>
2025-07-07 15:05:46 +0800

    完成了搜索功能和新增用户功能

---
+++ src/components/Form/UserForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <el-dialog
+      :title="title"
+      v-model="dialogVisible"
+      width="30%"
+  >
+    <el-form :model="formData" ref="formRef" :rules="rules">
+      <el-form-item
+          v-for="item in fields"
+          :key="item.prop"
+          :label="item.label"
+          :prop="item.prop"
+      >
+        <el-input v-model="formData[item.prop]" placeholder="请输入内容"></el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="closeDialog">取消</el-button>
+        <el-button type="primary" @click="submitForm">提交</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import { ref, defineProps, defineEmits, watch } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "新增用户",
+  },
+  fields: {
+    type: Array,
+    required: true,
+  },
+  visible: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(['update:visible', 'submit']);
+
+const dialogVisible = ref(false);
+const formData = ref({});
+const formRef = ref(null);
+
+const rules = {
+  name: [
+    { required: true, message: '请输入姓名', trigger: 'blur' },
+  ],
+  address: [
+    { required: true, message: '请输入地址', trigger: 'blur' },
+  ],
+  // 其他字段的规则
+};
+
+watch(() => props.visible, (newVal) => {
+  dialogVisible.value = newVal;
+});
+
+const closeDialog = () => {
+  emit('update:visible', false);
+};
+
+const submitForm = () => {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      emit('submit', formData.value);
+      closeDialog();
+    } else {
+      console.error('表单验证失败');
+    }
+  });
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
